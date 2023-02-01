12a4a4bf70e3f72ecbde8f8d633257b2d5fa2a3f
Shambles_SM <115082072+ShamblesSM@users.noreply.github.com>
2023-02-01 15:07:21 +0800

    Actually fix largestGap crash on LOVE 11.4
    Co-Authored-By: Aetzon <107924805+FredZachary@users.noreply.github.com>

--- src/SphereGroup.lua
+++ src/SphereGroup.lua
@@ -718,8 +718,8 @@ function SphereGroup:matchAndDeleteEffect(position, effect)
 		score = score + (1000 * (self.sphereChain.combo - 1))
     end
     local gapbonus
-	local largestGap = gaps and math.max(unpack(gaps))
 	if #gaps > 0 then
+		local largestGap = math.max(unpack(gaps))
 		if largestGap and largestGap < 20 --[[or not preOct2012GapScoring]] then
 			gapbonus = math.ceil(((1-(largestGap-0.5)/9.5)*1000)*math.min(1, #gaps) / 10) * 10 -- interpolate from 100,000 pts at 0,5 ball gap to 0 at 10 ball gap
 		else
