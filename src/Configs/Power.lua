f4e6a7a56f2e37521c9cf0560ec3e89f05e22eae
Shambles_SM <115082072+ShamblesSM@users.noreply.github.com>
2023-02-05 00:11:21 +0800

    Fix crash on profileless launches

--- src/Configs/Power.lua
+++ src/Configs/Power.lua
@@ -26,6 +26,9 @@ end
 ---Updates the currentLevel value.
 function Power:updateCurrentLevel()
     local profile = _Game:getCurrentProfile()
+    if not profile then
+        return
+    end
     if profile:getPowerLevel(self._name) > self.maxLevel then
         _Log:printt("Power", "Power "..self._name.." is over max level!")
         self.currentLevel = self.maxLevel
