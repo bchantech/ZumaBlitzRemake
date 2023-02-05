3a430d8a7d1bd2f0c31a96f17d3d95dd6627e663
Shambles_SM <115082072+ShamblesSM@users.noreply.github.com>
2023-02-05 17:00:28 +0800

    Add food sprites

---
+++ games/ZumaBlitzRemake/generate_food_sprites.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+d = {
+    "path": "",
+    "frame_size": {"x": 100, "y": 98},
+    "states": [
+        {
+            "pos": {
+                "x": 0,
+                "y": 0
+            },
+            "frames": {
+                "x": 1,
+                "y": 1
+            }
+        }
+    ],
+    "internal": False,
+    "batched": False
+}
+
+for p,n,fs in os.walk("./images/food_items"):
+    for filename in fs:
+        print(filename[:-4])
+        j = open(f"./sprites/food_items/{filename[:-4]}.json", "w")
+        d["path"] = f"./sprites/images/{filename}"
+        j.write(json.dumps(d, indent=4))
+        j.close()
