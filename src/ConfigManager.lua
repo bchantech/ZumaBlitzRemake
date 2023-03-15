a6d621f07a26f79d4acd9bc308436a7bbfe9aa58
jakubg1 <24206305+jakubg1@users.noreply.github.com>
2023-03-15 04:03:02 +0100

    ! Changeable Discord Application ID for games
    Changes in main game config (config.json):
    - In the `richPresence` section, a new field has been added: `applicationID`.
      - This field is optional. If specified, this will be the application ID shown when the player is playing that game, instead of a default OpenSMCE application.

--- src/ConfigManager.lua
+++ src/ConfigManager.lua
@@ -188,6 +188,12 @@ function ConfigManager:isRichPresenceEnabled()
 	return self.config.richPresence.enabled
 end
 
+---Returns the Rich Presence Application ID for this game, if it exists.
+---@return string?
+function ConfigManager:getRichPresenceApplicationID()
+	return self.config.richPresence.applicationID
+end
+
 
 
 ---Gets the level number which the checkpoint points to.
